231fefd65da5062fb8d2a4f4ecd9695ce2ada852
Mohammed Rayaan Pasha <mohdrayaanpasha@gmail.com>
2024-06-03 15:39:08 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/solacecraft-prototype/', // Ensure this matches your repo name
+  plugins: [react()]
+});
